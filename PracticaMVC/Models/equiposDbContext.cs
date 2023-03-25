3f814c93cd727b772c755b18e1cc83f62d7e5163
musicnestor <luis.mendoza2@catolica.edu.sv>
2023-03-25 11:46:08 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext:DbContext
+    {
+        public equiposDbContext(DbContextOptions<equiposDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<marcas> marcas { get; set; }
+    }
+}
